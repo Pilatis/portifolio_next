b3d8c1f8280ebdab3025bbedfd0f7cb5e33b73f9
Lucas Pilati <pilatilucas@gmail.com>
2025-01-02 01:37:17 -0300

    bugs corrections

--- components/Approach/index.tsx
+++ components/Approach/index.tsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import { CardCanvasReveal } from '@/components/ui/CardCanvasReveal';
+import React from 'react';
+import { CardDemo as CardApproach } from '@/components/ui/CardApproach';
+import { approachItems } from '@/data/approachItems';
 
 const Approach: React.FC = () => {
   return (
-    <div className="py-20" id="">
-        <h1>My</h1>
+    <div className="w-full py-20" id="">
+      <h1 className="heading">
+        My
         <span className="text-purple">Approach</span>
-        <CardCanvasReveal />
+      </h1>
+      <div className="py-20 flex flex-col lg:flex-row items-center justify-center bg-white dark:bg-black w-full gap-4 mx-auto px-8">
+        {approachItems.map((item) => (
+          <CardApproach
+            key={item.id}
+            title={item.title}
+            text={item.description}
+            image={item.image}
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Approach
+export default Approach;
