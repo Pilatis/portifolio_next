a27221bd9d8516762a9c7ec7bf2cc24236f415f8
Lucas Pilati <pilatilucas@gmail.com>
2025-01-14 22:07:56 -0300

    more adjusts in layout

--- components/Approach/index.tsx
+++ components/Approach/index.tsx
@@ -5,13 +5,11 @@ import { GridBackgroundDemo } from '../ui/GridBackgroundDemo';
 
 const Approach: React.FC = () => {
   return (
-    <div className="w-full relative py-20 flex items-center justify-center flex-col gap-10" id="">
-    <div className="w-screen lg:h-[800px] h-min absolute top-5 left-10">
-        <img className="w-screen bg-cover bg-center opacity-8" src="/grid.svg" alt="grid" />
-      </div>
-      <h1 className="heading">
+    <div className="w-full relative py-20 flex items-center justify-center flex-col gap-10 z-10" id="">
+       <GridBackgroundDemo />
+      <h1 className="heading z-10">
         My
-        <span className="text-purple">Approach</span>
+        <span className="text-purple ml-2">Approach</span>
       </h1>
       <div className="py-5 relative flex flex-col lg:flex-row items-center justify-center w-full gap-4 mx-auto px-8">
         {approachItems.map((item) => (
--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -7,25 +7,25 @@ import { SocialMediaItemsTypes } from '@/types/socialMediaItemsTypes';
 
 const Footer: React.FC = () => {
   return (
-    <div className="w-full py-20 flex flex-col items-center justify-center">
-      <div className="w-full lg:max-w-[640px] mb-16 flex flex-col items-center justify-center gap-10">
+    <div className="w-full relative pt-10 pb-20 flex flex-col items-center justify-center z-10">
+      <div className="w-full lg:max-w-[640px] md:mb-10 mb-16 flex flex-col items-center justify-center gap-10">
         <h1 className="heading text-center">
           Ready to take <span className="text-purple">your</span> digital
           presence to the next level?
         </h1>
-        <p className="text-sm md:text-base font-light">
+        <p className="text-sm md:text-base text-center font-light">
           Reach out to me today and let&apos;s discuss how i can help you
           achieve your goals
         </p>
       </div>
-      <MagicButton iconPosition="right" icon={<FaLocationArrow size="15px" />}>Let&apos;s get in touch</MagicButton>
-      <div className="w-full flex items-center flex-col md:gap-8 lg:justify-between mt-16">
-        <p className="md:text-base text-sm md:font-normal font-light">
+      <MagicButton otherClasses="md:mb-10" iconPosition="right" icon={<FaLocationArrow size="15px" />}>Let&apos;s get in touch</MagicButton>
+      <div className="w-full flex items-center justify-center lg:justify-between flex-col lg:flex-row md:gap-8 gap-3 mt-14 md:mt-10">
+        <p className="w-full md:text-base text-sm md:font-normal text-center font-light">
            Copyright Ⓒ 2025 Lucas
         </p>
-        <div className="w-full flex items-center md:gap-3 gap-6">
+        <div className="w-full inline-flex items-center justify-center md:gap-3 text-center gap-4">
            {socialMediaItems.map((item: SocialMediaItemsTypes) => (
-            <div key={item.id}>
+            <div key={item.id} className="w-10 h-10 cursor-pointer flex items-center justify-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
               <Image src={item.image} alt={item.name} width={20} height={20} />
             </div>
            ))}
